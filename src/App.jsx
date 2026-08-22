4f723264d8ab18bc6d5dddf5b47e22126cdb4285
Carlos R. Ueda <carlos.ueda@gmail.com>
2026-08-22 08:07:40 -0300

    Adding behaviour for onClick for the TabButton

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
-import { CORE_CONCEPTS } from './data.js';
+import { useState } from 'react';
+import { CORE_CONCEPTS, EXAMPLES } from './data.js';
 import Header from './components/Header/Header.jsx';
 import CoreConcept from './components/CoreConcept.jsx';
 import TabButton from './components/TabButton.jsx';
 import './components/Header/Header.css';
 
 function App() {
-  const onSelect = () => {
-    console.log('Tab selected');
+  const [selectedComponent, setTabContent] = useState('components');
+  const onSelect = (component) => { 
+    setTabContent(component);
   };
   return (
     <div>      
@@ -23,11 +25,18 @@ function App() {
         <section id="examples">
           <h2>Examples</h2>
           <menu>
-            <TabButton onSelect={onSelect}>Components</TabButton>
-            <TabButton onSelect={onSelect}>JSX</TabButton>
-            <TabButton onSelect={onSelect}>Props</TabButton>
-            <TabButton onSelect={onSelect}>State</TabButton>
+            <TabButton onSelect={() => onSelect('components')}>Components</TabButton>
+            <TabButton onSelect={() => onSelect('jsx')}>JSX</TabButton>
+            <TabButton onSelect={() => onSelect('props')}>Props</TabButton>
+            <TabButton onSelect={() => onSelect('state')}>State</TabButton>
           </menu>
+          <div id="tab-content">
+            <h3>{EXAMPLES[selectedComponent].title}</h3>
+            <p>{EXAMPLES[selectedComponent].description}</p>
+            <code>
+              {EXAMPLES[selectedComponent].code}
+            </code>
+          </div>
         </section>
       </main>
     </div>
